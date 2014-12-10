31ae483070b631ae2555001e1a5f2e7d427334e0
Michael Chen <cwchen123@gmail.com>
2014-12-10 20:03:56 +0800

    add HTTPS support

--- manage.py
+++ manage.py
@@ -3,12 +3,14 @@ from app import create_app
 from flask.ext.script import Manager, Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
+# TODO: solve HTTPS connection with Manager
+# manager = Manager(app)
 
-def make_shell_context():
-    return dict(app=app)
+# def make_shell_context():
+#     return dict(app=app)
 
-manager.add_command('shell', Shell(make_context=make_shell_context))
+# manager.add_command('shell', Shell(make_context=make_shell_context))
 
 if __name__ == '__main__':
-    manager.run()
+    # enable safer HTTPS connection
+    app.run(ssl_context='adhoc')
